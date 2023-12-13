aa3a6f9f9028cd4336e31edea575f98c342d0ac5
adamaconguli <waltson2003@gmail.com>
2023-12-13 15:17:52 +0800

    add error handling and comments

--- src/assets/result.js
+++ src/assets/result.js
@@ -1,18 +1,31 @@
-const body = document.querySelector('body')
 const input = document.querySelector('input')
 const button = document.querySelector('button')
 const link = document.querySelector('a')
+const invalid = document.querySelector('p')
 
+// submit event handler
 button.addEventListener('click', (event) => {
+  // prevent refresh
   event.preventDefault()
 
+  // call api
   fetch(`/api?weight=${input.value}`)
     .then((res) => res.text())
     .then((data) => {
+      // create file link
       link.href = data
       link.textContent = data
+
+      // validation
+      if (data.length === 0) {
+        invalid.style.display = 'block'
+      } else {
+        invalid.style.display = 'none'
+      }
     })
     .catch((err) => {
+      // handle error
+      invalid.style.display = 'block'
       console.log(err)
     })
 })
